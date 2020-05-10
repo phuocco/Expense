ce18b1c2d8f43b3b92fb18d54625bb755dac7c02
Phuoc Nguyen <phuocco2901@gmail.com>
2020-05-10 15:32:32 +0700

    done

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -4,33 +4,38 @@ import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
-  TransactionList(this.transactions);
+  final Function deleteTransaction;
+  TransactionList(this.transactions, this.deleteTransaction);
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300,
+      height: 520,
       child: ListView.builder(
         itemBuilder: (context, index) {
           return Card(
             elevation: 6,
             margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
             child: ListTile(
-              leading: CircleAvatar(
-                radius: 30,
-                child: Padding(
-                  padding: EdgeInsets.all(5),
-                  child:
-                      FittedBox(child: Text('\$${transactions[index].amount}')),
+                leading: CircleAvatar(
+                  radius: 30,
+                  child: Padding(
+                    padding: EdgeInsets.all(5),
+                    child: FittedBox(
+                        child: Text('\$${transactions[index].amount}')),
+                  ),
                 ),
-              ),
-              title: Text(
-                transactions[index].title,
-                style: Theme.of(context).textTheme.title,
-              ),
-              subtitle: Text(
-                DateFormat.yMMMd().format(transactions[index].date),
-              ),
-            ),
+                title: Text(
+                  transactions[index].title,
+                  style: Theme.of(context).textTheme.title,
+                ),
+                subtitle: Text(
+                  DateFormat.yMMMd().format(transactions[index].date),
+                ),
+                trailing: IconButton(
+                  icon: Icon(Icons.delete),
+                  color: Theme.of(context).errorColor,
+                  onPressed: () => deleteTransaction(transactions[index].id),
+                )),
           );
         },
         itemCount: transactions.length,
